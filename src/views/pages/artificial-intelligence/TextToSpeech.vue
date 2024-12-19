8b3ee2c7dcfc9c02667c14bf188d2d042f8d38e3
longnv-dev <longcdvt0403@gmail.com>
2024-12-20 00:03:30 +0700

    commit

--- src/views/pages/artificial-intelligence/TextToSpeech.vue
+++ src/views/pages/artificial-intelligence/TextToSpeech.vue
@@ -7,7 +7,7 @@
         <img src="../../../assets/uploads/ai_assistant2.gif" alt="" width="300px" v-if="audioPlaying">
         <img src="../../../assets/uploads/ai_assistant2-0000.jpg" alt="" width="300px" v-else>
     </div>
-    <div class="col-3">
+    <!-- <div class="col-3">
         <form action="" method="post" @submit.prevent="submitRequest()">
             <a-input placeholder = "Nhập ..." allowClear v-model:value = "form.text"></a-input>
             <a-select :options = "languages" class="mt-2 w-100" placeholder = "-- Chọn ngôn ngữ --" allowClear v-model:value = "form.language"></a-select>
@@ -16,8 +16,13 @@
             <button class="btn btn-sm btn-primary mt-2">Gửi</button>
         </form>
         
-        <!-- <audio :src="audioURL" v-if = "audioURL" class="mt-3" controls></audio> -->
-        <!-- <pre>{{ audioURL }}</pre> -->
+        
+    </div> -->
+    {{ audioResultUrl }}
+    <div class="mt-3 text-center">
+        <h5>Ghi âm</h5>
+        <button class="btn btn-success btn-sm" v-if="isRecording" @click="stopRecording">Dừng lại</button>
+        <button class="btn btn-primary btn-sm" v-else @click="startRecording">Ghi âm</button>
     </div>
 </template>
 
@@ -26,73 +31,55 @@ import axios from 'axios';
 import { reactive, ref } from 'vue';
 import Loading from '../../components/Loading.vue';
 import { message } from 'ant-design-vue';
+import RecordRTC from "recordrtc";
 
+const isRecording = ref(false)
+const recorder = ref(null);
 const audioURL = ref(null);
 const textResult = ref(null);
 const isLoading = ref(false);
 const audioPlaying = ref(false)
 const apiURL = import.meta.env.VITE_APP_API;
+const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+const audioResultUrl = ref(null)
 const form = reactive({
     text: '',
     language: null,
     voice: null,
     model: null
 })
-const languages = [
+
+
+const startRecording = () => {
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    isRecording.value = true;
+    recorder.value = new RecordRTC(stream, { type: "audio", mimeType: isIOS ? "audio/m4a" : "audio/wav" });
+    recorder.value.startRecording();
+  });
+};
+
+const stopRecording = () => {
+    isRecording.value = false;
+    isLoading.value = true;
+    recorder.value.stopRecording(async () => {
+
+    const blob = recorder.value.getBlob();
+    const formData = new FormData();
+
+    formData.append("audio", blob, "recorded_audio.wav");
+    let response = await axios.post(apiURL + 'convert-speech-to-text', formData);
+    
+    console.log(response);
+    
+    if(response.status) 
     {
-        value: 'vi',
-        label: 'Tiếng Việt'
-    },
-    {
-        value: 'en',
-        label: 'Tiếng Anh'
+      isLoading.value = false;
+      audioResultUrl.value = response.data.data.url;
+      playAudio(audioResultUrl.value)
     }
-];
+  });
+};
 
-const voices = [
-    {
-        value : 'alloy',
-        label : 'Alloy'
-    },
-
-    {
-        value : 'echo',
-        label : 'Echo'
-    },
-
-    {
-        value : 'fable',
-        label : 'Fable'
-    },
-
-    {
-        value : 'onyx',
-        label : 'Onyx'
-    },
-
-    {
-        value : 'nova',
-        label : 'Nova'
-    },
-
-    {
-        value : 'shimmer',
-        label : 'Shimmer'
-    },
-];
-
-
-const TTSModels = [
-    {
-        value: 'tts-1',
-        label: 'Text to speech (15$/1M characters)'
-    },
-
-    {
-        value: 'tts-1-hd',
-        label: 'Text to speech HD (30$/1M characters)'
-    }
-];
 
 const submitRequest = async() => {
     isLoading.value = true;
