d217f7e027551c11e40007571c2308956622e3bb
Nguyễn Văn Long <longnv@langtech.vn>
2025-05-08 16:42:02 +0700

    commit

--- src/utils/echo.js
+++ src/utils/echo.js
@@ -7,6 +7,7 @@ const echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT || 8888,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 8888,
     forceTLS: false,
     enabledTransports: ['ws', 'wss'],
 });
